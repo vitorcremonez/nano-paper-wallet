3c1d639c18f7fe9b6b8ee510d7b6079c996c380d
Vitor Cremonez <vitorcremonez@gmail.com>
2017-12-31 17:27:07 -0200

    Adding reducerForm to the application.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import {
+    reducer as formReducer,
+} from 'redux-form';
+import {
+    combineReducers,
+} from 'redux';
+
+const rootReducer = combineReducers({
+    form: formReducer,
+});
+
+export default rootReducer;
