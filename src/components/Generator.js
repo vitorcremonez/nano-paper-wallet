937650d66410f27763ae59be97a610ce7ae3f715
Vitor Cremonez <vitorcremonez@gmail.com>
2017-12-31 17:28:04 -0200

    Paper Wallet Generator component adding public_key and seed to state of component.

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -5,29 +5,117 @@ import {
     Row,
     Input,
 } from 'react-materialize';
-
+import { QRCode } from 'react-qr-svg';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 
 class Generator extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            public_key: null,
+            seed: null,
+        };
+    }
+
+    renderPaperWallet() {
         return (
-            <div>
-                <Row>
-                    <Input s={12} label="Public account key" validate>
-                        <Icon>account_balance_wallet</Icon>
-                    </Input>
-                </Row>
-                <Row>
-                    <Input s={12} label="Seed" validate>
-                        <Icon>lock</Icon>
-                    </Input>
-                </Row>
-                <Button waves='light' className="cyan" onClick={() => alert("This function will be available soon!")}>
-                    <Icon left>note_add</Icon>
-                    Generate your <b>paper wallet</b>
-                </Button>
+            <div style={{backgroundColor: "#AAA"}}>
+                <div>
+                    <b>Public Key:</b>
+                </div>
+                <div>
+                    <QRCode
+                        bgColor="#FFFFFF"
+                        fgColor="#000000"
+                        level="Q"
+                        style={{ width: 128 }}
+                        value={ this.state.public_key }
+                    />
+                </div>
+                <div>
+                    { this.state.public_key }
+                </div>
+
+                <div>
+                    <b>Seed:</b>
+                </div>
+                <div>
+                    <QRCode
+                        bgColor="#FFFFFF"
+                        fgColor="#000000"
+                        level="Q"
+                        style={{ width: 128 }}
+                        value={ this.state.seed }
+                    />
+                </div>
+                <div>
+                    { this.state.seed }
+                </div>
             </div>
         );
     }
+
+    onSubmit(values) {
+        console.log(values);
+        this.setState({
+            public_key: values.public_key,
+            seed: values.seed,
+        });
+    }
+
+    renderInput(field){
+        return (
+            <Row>
+                <Input {...field} {...field.input} {...field.meta}>
+                    <Icon>{field.icon}</Icon>
+                </Input>
+            </Row>
+        );
+    }
+
+    renderInputForm() {
+        return (
+            <div>
+                <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+                    <Field
+                        name="public_key"
+                        type="text"
+                        icon="account_balance_wallet"
+                        s={12}
+                        label="Public account key"
+                        validade={true}
+                        component={this.renderInput}
+                    />
+                    <Field
+                        name="seed"
+                        type="text"
+                        icon="lock"
+                        s={12}
+                        label="Seed"
+                        validade={true}
+                        component={this.renderInput}
+                    />
+                    <Button waves='light' className="cyan">
+                        <Icon left>note_add</Icon>
+                        Generate your <b>paper wallet</b>
+                    </Button>
+                </form>
+            </div>
+        );
+    }
+
+    render() {
+        if(this.state.public_key && this.state.seed){
+            return this.renderPaperWallet();
+        }
+        return this.renderInputForm();
+    }
 }
 
-export default Generator;
+
+export default reduxForm({
+    form: 'Generator'
+})(
+    connect(null)(Generator)
+);
