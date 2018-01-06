2d95a2ab52ae4a8cb81e6f7a0a16183ab3e1415f
Vitor Cremonez <vitorcremonez@gmail.com>
2018-01-05 23:12:18 -0200

    Adding some steps on tutorial component.

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -7,8 +7,11 @@ class Tutorial extends Component {
     render() {
         return (
             <div>
-                <h4>Make Paper Wallets to Keep Your RaiBlocks Addresses Safe.</h4>
-                <h5>Follow this simple steps to generate your raiblocks paper wallet in a safe way.</h5>
+                <h4>Make your Paper Wallet to Keep Your RaiBlocks and Keys Safe.</h4>
+                <h5>Follow these simple steps to generate your raiblocks paper wallet in a safe place.</h5>
+                <h6>
+                    Client side generator. All functions of paper wallet generator is runned in client side to help keep your new paper wallet far from another people.
+                </h6>
                 <br/>
                 <h5>
                     <b>Intructions:</b>
@@ -21,8 +24,11 @@ class Tutorial extends Component {
                     <li>Access Paper Wallet Generator in a new <Icon>visibility_off</Icon> Private Window in your browser.</li>
                     <li>Disconnect your <Icon>wifi</Icon> internet connection.</li>
                     <li>Click in "<Icon>gesture</Icon> Generate your wallet now!".</li>
+                    <li>Move cursor around screen <Icon>gesture</Icon> to help generate a good randomic entropy.</li>
                     <li>Click in "<Icon>note_add</Icon> Create your paper wallet".</li>
                     <li>Click in "<Icon>print</Icon> Print your paper wallet".</li>
+                    <li>Close <Icon>close</Icon> the browser and restart computer.</li>
+                    <li>Note: You can <Icon>update</Icon> reinstall OS to feel safe.</li>
                 </ol>
                 <br/>
                 <h6>
@@ -34,6 +40,8 @@ class Tutorial extends Component {
                     <li>Add your <Icon>lock</Icon> Seed, <Icon>account_balance_wallet</Icon> Account and <Icon>call_received</Icon> Public key.</li>
                     <li>Click in "<Icon>note_add</Icon> Create your paper wallet".</li>
                     <li>Click in "<Icon>print</Icon> Print your paper wallet".</li>
+                    <li>Close <Icon>close</Icon> the browser and restart computer.</li>
+                    <li>Note: You can <Icon>update</Icon> reinstall OS to feel safe.</li>
                 </ol>
             </div>
         );
