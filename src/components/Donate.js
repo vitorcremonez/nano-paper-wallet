7b2dfd93af8de07e27178c93fe73fc8e3dfa9b07
Vitor Cremonez <vitorcremonez@gmail.com>
2018-01-09 11:48:12 -0200

    Adding account to receive donations to improve paper wallet.

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,31 +1,32 @@
 import React, {Component} from 'react';
-import { QRCode } from 'react-qr-svg';
+import {QRCode} from 'react-qr-svg';
 
 class Donate extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            donate_public_key: "xrb_xxx",
+            donate_public_key: "xrb_1b7bhxbz7n66iie8ygsde87ajoiyy38xbxi5zywsw8gncxhswso61191n1d4",
         };
     }
 
     render() {
         return (
-            <div className="bg-lightsmoke responsive-text" style={{padding:32}}>
+            <div className="bg-lightsmoke responsive-text" style={{padding: 32}}>
                 <h6>
                     <b>Donate XRB and help us to improve the paper wallet!</b>
                 </h6>
                 <br/>
-                <div>
+                <div style={{padding: 10, background: "white", width: 128, margin: "auto"}}>
                     <QRCode
                         bgColor="#FFFFFF"
                         fgColor="#000000"
                         level="Q"
-                        style={{ width: 128 }}
                         value={ this.state.donate_public_key }
                     />
                 </div>
-                { this.state.donate_public_key }
+                <h6>
+                    { this.state.donate_public_key }
+                </h6>
             </div>
         );
     }
